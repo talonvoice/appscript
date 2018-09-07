acb7847e357238e59b79c14778d69efff324f202
Ryan Hileman <lunixbochs@gmail.com>
2018-09-06 20:12:33 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Appscript',
+      version='1.0',
+      description='macOS Automation Framework',
+      author='Ryan Hileman',
+      author_email='python@bochs.info',
+      url='https://github.com/talonvoice/appscript/',
+      packages=['appscript', 'appscript.aem'],
+)
