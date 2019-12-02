bfc2e50f0219e910dabeaacc31590d7b1e2cb33a
Ryan Hileman <lunixbochs@gmail.com>
2019-12-02 12:03:29 -0800

    reformat keywordwrapper.py

--- appscript/keywordwrapper.py
+++ appscript/keywordwrapper.py
@@ -5,40 +5,40 @@
 # to be created by referring to the exported 'k' variable; e.g. k.document, k.ask, k.name, k.String.
 
 class Keyword:
-	"""A class/property/enumerator/type name."""
-	
-	def __init__(self, name): 
-		self.AS_name = name
-	
-	def __repr__(self): 
-		return 'k.{}'.format(self.AS_name)
-	
-	def __hash__(self): 
-		return hash(self.AS_name)
-	
-	def __eq__(self, val):
-		return val.__class__ == self.__class__ and val.AS_name == self.AS_name
-	
-	def __ne__(self, val):
-		return not self.__eq__(val)
-	
-	def __bool__(self):
-		return self.AS_name != 'missing_value'
-	
-	name = property(lambda self:self.AS_name)
+    """A class/property/enumerator/type name."""
+
+    def __init__(self, name):
+        self.AS_name = name
+
+    def __repr__(self):
+        return 'k.{}'.format(self.AS_name)
+
+    def __hash__(self):
+        return hash(self.AS_name)
+
+    def __eq__(self, val):
+        return val.__class__ == self.__class__ and val.AS_name == self.AS_name
+
+    def __ne__(self, val):
+        return not self.__eq__(val)
+
+    def __bool__(self):
+        return self.AS_name != 'missing_value'
+
+    name = property(lambda self:self.AS_name)
 
 
 class _KeywordShim(object):
-	""" Infinite namespace 'containing' all possible class/property/enumerator/type names. """
-	
-	def __getattr__(self, name): 
-		if name.startswith('__') and name.endswith('__'):
-			return object.__getattr__(self, name)
-		else:
-			return Keyword(name)
-	
-	def __repr__(self):
-		return 'k'	
+    """ Infinite namespace 'containing' all possible class/property/enumerator/type names. """
+
+    def __getattr__(self, name):
+        if name.startswith('__') and name.endswith('__'):
+            return object.__getattr__(self, name)
+        else:
+            return Keyword(name)
+
+    def __repr__(self):
+        return 'k'
 
 
 k = _KeywordShim()
